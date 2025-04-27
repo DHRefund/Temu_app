a1016fe6e4c89e50e2b87abe11d5d2ffa3c54108
PhamTinh <tinh1235a@gmail.com>
2025-04-27 16:25:54 +0900

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,27 @@
+import { getCurrentSession } from "@/actions/auth";
+import { getWheelOfFortuneConfiguration } from "@/actions/wheel-of-fortune-actions";
+import SalesCampaignBanner from "@/components/layout/SalesCampaignBanner";
+import WheelOfFortune from "@/components/layout/WheelOfFortune";
+import ProductGrid from "@/components/product/ProductGrid";
+import { getAllProducts } from "@/sanity/lib/client";
+
+const Home = async () => {
+  const { user } = await getCurrentSession();
+
+  const products = await getAllProducts();
+
+  const { randomProducts, winningIndex } = await getWheelOfFortuneConfiguration();
+  // console.log("randomProducts", randomProducts);
+  // console.log("winningIndex", winningIndex);
+  return (
+    <div>
+      {/* <WheelOfFortune products={randomProducts} winningIndex={winningIndex} /> */}
+
+      <section className="container mx-auto py-8">
+        <ProductGrid products={products} />
+      </section>
+    </div>
+  );
+};
+
+export default Home;
